62cdd77fb1b057d7ec0f3914d81d85bc88ac6fdc
TheyKay <murraykyleb@gmail.com>
2020-08-19 21:19:05 -0600

    added select forms

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
-let containerEl = $('#container');
-// display current date and time
-let todate = $('<h3>');
-todate.text(moment().format('dddd, DD MMMM'));
-containerEl.append(todate);
 // display current day in header jumbotron
 $('#today').text(moment().format('dddd'));
+let todayP = $('#currentDay');
+// display current date and time
+todayP.text(moment().format('DD MMMM'));
+const selectStart = $('#selectStart');
+const selectEnd = $('#selectEnd');
+
+function formatTime(number) {
+    let formatted = '';
+    if (number === 0) {
+        formatted = '12 AM';
+    } else if (number >= 1 && number < 12) {
+        formatted = number + ' AM'; 
+    } else if (number === 12) {
+        formatted = number + ' PM';
+    } else {
+        formatted = number - 12 + ' PM';
+    }
+    return formatted;
+}
+
+for (let i = 0; i < 24; i++) {
+    const optionStart = $('<option>');
+    const optionEnd = $('<option>');
+    
+    optionStart.text(formatTime(i));
+    optionStart.attr('id', i);
+    selectStart.append(optionStart);
+    
+    optionEnd.text(formatTime(i));
+    optionEnd.attr('id', i);
+    selectEnd.append(optionEnd);
+}
